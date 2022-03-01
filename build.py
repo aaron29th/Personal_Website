ec6326ea5db1430061dc7d54b2223751b4d02aae
aaron rosser <aaron25th@gmail.com>
2022-03-01 17:43:17 +0000

    Added alternate email and pgp key

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 import sys
 import jsonpickle
 
+from urllib.parse import quote
 from staticjinja import Site
 
 
@@ -9,10 +10,19 @@ if __name__ == "__main__":
     with open('projects.json', 'r') as f:
         projects = jsonpickle.decode(f.read())
 
+    pgp_key = None
+    with open('resources/Aaron_Rosser_aaron@aaronrosser.xyz-0x67765DE3715DB0EB-pub.asc', 'r') as f:
+        pgp_key = f.read()
+
     site = Site.make_site(
         outpath="build",
         env_globals={
-            'projects': projects
+            'projects': projects,
+            'pgp_key': {
+                'data': pgp_key,
+                'encoded_data': quote(pgp_key),
+                'filename': 'Aaron_Rosser_aaron@aaronrosser.xyz-0x67765DE3715DB0EB-pub.asc'
+            }
         }
     )
     auto_reload = 'reload' in sys.argv
